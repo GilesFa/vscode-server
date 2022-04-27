d9584cf97368757c7d7aaf657b6c633c33fa90b7
GilesFa <vsopk999@gmail.com>
2022-04-27 22:23:51 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ docker exec -it code-server sudo chown -R ${user}:${user} ${project_path}
 echo "set contanier ok"
 
 #3. 容器系統更新與安裝python3
-docker exec -it code-server sudo apt update
+docker exec -it code-server sudo apt-get update -y
 docker exec -it code-server sudo apt-get install python3 pip flask flask_cors -y
 docker exec -it code-server pip --version
 echo "vscode update & install python3 ok"
